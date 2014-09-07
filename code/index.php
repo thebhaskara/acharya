47a2c1a1d0c31249e90570de817fcd9f023cfe53
thebhaskara <thebhaskaradeveloper@gmail.com>
2014-09-07 18:15:47 +0530

    Project setup

---
+++ code/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title is not decided yet</title>
+    </head>
+
+    <body>
+        this is our new project
+    </body>
+
+</html>
